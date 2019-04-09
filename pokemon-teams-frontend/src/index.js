62ca9d93168c228a82a077bbad1552d10785a6ae
Georgina Hoagland <georgina.hoagland@flatironschool.com>
2019-04-09 11:25:40 -0400

    after review

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,85 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const mainTag = document.querySelector('main')
+
+document.addEventListener('DOMContentLoaded', onLoad)
+
+function onLoad() {
+  mainTag.addEventListener('click', clickHandler)
+  fetch(TRAINERS_URL)
+    .then(r => r.json())
+    .then((trainerArr) => {
+      trainerArr.forEach(trainerObj => {
+        mainTag.innerHTML += trainerCard(trainerObj)
+        let ulTag = mainTag.querySelector(`.poke-${trainerObj.id}`)
+        trainerObj.pokemons.forEach(pokemon => {
+          ulTag.innerHTML += pokemonTemplate(pokemon)
+        })
+      }
+    )
+  })
+}
+
+function clickHandler(evt) {
+  if(evt.target.innerText === 'Add Pokemon') {
+    addPokemon(evt.target.dataset.trainerId)
+  } else if (evt.target.innerText === 'Release') {
+    removePokemon(evt);
+  }
+}
+
+function removePokemon(evt) {
+  const pokemonId = evt.target.dataset.pokemonId
+  fetch(`${POKEMONS_URL}/${pokemonId}`, {
+    method: 'DELETE'
+  }).then(r => r.json())
+    .then(pokemon => {
+      if(pokemon.id) {
+        evt.target.parentElement.remove()
+      } else {
+        throw new Error('Pokemon was not sucessfully deleted')
+      }
+    })
+    .catch(console.error)
+}
+
+
+function addPokemon(trainerId) {
+  fetch(POKEMONS_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      "trainer_id": trainerId
+    })
+
+  }).then(r => r.json())
+    .then((pokemon) => {
+      if(pokemon.trainer_id) {
+        let ulTag = mainTag.querySelector(`.poke-${pokemon.trainer_id}`)
+        ulTag.innerHTML += pokemonTemplate(pokemon)
+      } else {
+        alert(pokemon.error)
+      }
+    })
+}
+
+const trainerCard = (trainerObj) => {
+  return `
+    <div class="card" data-id="${trainerObj.id}"><p>${trainerObj.name}</p>
+      <button data-trainer-id="${trainerObj.id}">Add Pokemon</button>
+      <ul class="poke-${trainerObj.id}">
+
+      </ul>
+    </div>
+  `
+}
+
+const pokemonTemplate = (pokemon) => {
+  return `
+    <li>${pokemon.nickname } (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>
+`
+}
